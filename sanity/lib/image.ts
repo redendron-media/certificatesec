c23a060e74d82e820795ef477be5792d62e52903
Ramit <ramitsingh12@gmail.com>
2025-06-01 15:27:12 +0530

    error

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,11 +1,12 @@
-import createImageUrlBuilder from '@sanity/image-url'
+import createImageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-import { dataset, projectId } from '../env'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "7s3z43h2";
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 // https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId, dataset })
+const builder = createImageUrlBuilder({ projectId, dataset });
 
 export const urlFor = (source: SanityImageSource) => {
-  return builder.image(source)
-}
+  return builder.image(source);
+};
